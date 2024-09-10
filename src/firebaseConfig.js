d628b268ab805a29af2e233d2019282db31b0a1d
Hassan Ali <hassanalibhatti648@gmail.com>
2024-09-10 13:44:58 +0500

    Added Education and Designation Field in SigUp form

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,8 +1,8 @@
 // Import the necessary functions from Firebase SDKs
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";  // Correct import from firebase/auth
+import { getAuth } from "firebase/auth";
+import { getFirestore  } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDJWoR8_ufX1JWe78zPoruGFnBOA7zEqa8",
   authDomain: "medical-mind-9b44a.firebaseapp.com",
@@ -12,8 +12,7 @@ const firebaseConfig = {
   appId: "1:781623047154:web:99f4adb92865d2f952b16b"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);  // Correct initialization of auth
-
-// You can now use `auth` in your applicatio
+const auth = getAuth(app);
+const db = getFirestore(app);
+export { auth, db };
